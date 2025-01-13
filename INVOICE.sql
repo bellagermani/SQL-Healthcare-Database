30a1e8eca854d4c6041d96d38475f05c35ea5325
Bella <145384907+bellagermani@users.noreply.github.com>
2025-01-13 18:52:57 -0500

    Add files via upload

---
+++ INVOICE.sql
@@ -0,0 +1,125 @@
+-- (1 FILE, 30 RECORDS)
+-- DONE
+
+USE HealthcareDB;
+SET @PatientID = 3;
+
+-- 1
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-11-11', 320.00);
+
+-- 2
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-08-08', 20340.56);
+
+-- 3
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-01-01', 806.76);
+
+-- 4
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-04-14', 50200.01);
+
+-- 5
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-06-15', 105.55);
+
+-- 6
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-03-30', 20.70);
+
+-- 7
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-07-14', 751.86);
+
+-- 8
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-05-21', 5700.89);
+
+-- 9
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2023-03-12', 808.08);
+
+-- 10
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2023-09-13', 32067.56);
+
+-- 11
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-07-17', 233.50);
+
+-- 12
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-09-25', 102.03);
+
+-- 13
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-09-19', 87.33);
+
+-- 14
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-12-21', 6800.01);alter
+
+-- 15
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-01-23', 20045.54);
+
+-- 16
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-04-27', 345.84);
+
+-- 17
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-07-18', 113.34);
+
+-- 18
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-10-03', 713.69);
+
+-- 19
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2019-12-13', 4502.47);
+
+-- 20
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2018-11-15', 400.20);
+
+-- 21
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2019-09-26', 191.23);
+
+-- 22
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-01-22', 222.30);
+
+-- 23
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-12-22', 1203.76);
+
+-- 24
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2023-04-28', 435.00);alter
+
+-- 25
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2019-11-18', 899.59);alter
+
+-- 26
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2020-01-29', 655.89);
+
+-- 27
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2022-07-11', 2280.67);alter
+
+-- 28
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2021-06-23', 109.01);alter
+
+-- 29
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2019-10-24', 98.50);
+
+-- 30
+INSERT INTO Invoices (PatientID, InvoiceDate, TotalAmount)
+VALUES (@PatientID, '2018-09-05', 100.03);
---
+++ STAFF.sql
@@ -0,0 +1,124 @@
+-- (1 FILE, 30 RECORDS)
+-- DONE
+
+USE HealthcareDB;
+
+-- 1
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Ethan', 'Anderson', 'Cardiologist', 'e.anderson@gmail.com', '234-567-8901', 'MD458244', '2022-10-11');
+
+-- 2
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Olivia', 'Mitchell', 'Neurologist', 'o.mitchell@gmail.com', '876-543-2109', 'MD626303', '2020-08-09');
+
+-- 3
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Liam', 'Reynolds', 'Anesthesiologist', 'l.reynolds@gmail.com', '456-789-0123', 'MD152990', '2023-03-21');
+
+-- 4
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Ava', 'Martinez', 'Radiologist', 'a.martinez@gmail.com', '321-098-7654', 'MD557210', '2022-06-18');
+
+-- 5
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Noah', 'Thompson', 'Urologist', 'n.thompson@gmail.com', '789-012-3456', 'MD659297', '2019-04-14');
+
+-- 6
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Sophia', 'Sullivan', 'Cardiologist', 's.sullivan@gmail.com', '543-210-9876', 'MD666234', '2011-03-12');
+
+-- 7
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Jackson', 'Ramirez', 'Neurologist', 'j.ramirez@gmail.com', '987-654-3210', 'MD884356', '2020-01-19');
+
+-- 8
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Isabella', 'Phillips', 'Anesthesiologist', 'i.phillips@gmail.com', '210-987-6543', 'MD941709', '2021-11-13');
+
+-- 9
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Aiden', 'Turner', 'Radiologist', 'a.turner@gmail.com', '678-901-2345', 'MD774936', '2018-06-06');
+
+-- 10
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Mia', 'Patel', 'Urologist', 'm.patel@gmail.com', '890-123-4567', 'MD116263', '2019-07-20');
+
+-- 11
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Lucas', 'Price', 'Cardiologist', 'l.price@gmail.com', '345-678-9012', 'MD482209', '2015-02-11');
+
+-- 12
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Amelia', 'Foster', 'Neurologist', 'a.foster@gmail.com', '109-876-5432', 'MD997264', '2022-10-22');
+
+-- 13
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Mason', 'Carter', 'Anesthesiologist', 'm.carter@gmail.com', '543-210-9876', 'MD254678', '2023-06-15');
+
+-- 14
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Harper', 'Washington', 'Radiologist', 'h.washintgton@gmail.com', '987-654-3210', 'MD709006', '2022-11-11');
+
+-- 15
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Logan', 'Hayes', 'Urologist', 'l.hayes@gmail.com', '210-987-6543', 'MD123856', '2018-08-05');
+
+-- 16
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Evelyn', 'Jenkins', 'Cardiologist', 'e.jenkins@gmail.com', '678-901-2345', 'MD680521', '2017-07-07');
+
+-- 17
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Oliver', 'Russel', 'Neurologist', 'o.russel@gmail.com', '890-123-4567', 'MD120495', '2022-10-18');
+
+-- 18
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Abigail', 'Thevel', 'Anesthesiologist', 'a.thevel@gmail.com', '345-678-9012', 'MD587036', '2021-04-16');
+
+-- 19
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Elijah', 'Fisher', 'Radiologist', 'e.fisher@gmail.com', '109-876-5432', 'MD166423', '2020-03-01');
+
+-- 20
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Emily', 'Harris', 'Urologist', 'e.harris@gmail.com', '987-654-3210', 'MD769121', '2020-02-06');
+
+-- 21
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Carter', 'Rodriguez', 'Cardiologist', 'c.rodriguez@gmail.com', '210-987-6543', 'MD185623', '2023-01-12');alter
+
+-- 22
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Scarlett', 'Weaver', 'Neurologist', 's.weaver@gmail.com', '678-901-2345', 'MD987652', '2023-01-02');
+
+-- 23
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Caleb', 'Olson', 'Anesthesiologist', 'c.olson@gmail.com', '890-123-4567', 'MD369523', '2022-02-14');
+
+-- 24
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Chloe', 'Perry', 'Radiologist', 'c.perry@gmail.com', '345-678-9012', 'MD123455', '2021-10-04');
+
+-- 25
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Benjamin', 'Nguyen', 'Urologist', 'b.nguyen@gmail.com', '109-876-5432', 'MD833040', '2019-09-13');
+
+-- 26
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Grace', 'Powell', 'Cardiologist', 'g.powell@gmail.com', '543-210-9876', 'MD742124', '2018-09-16');
+
+-- 27
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Henry', 'Simpson', 'Neurologist', 'h.simpson@gmail.com', '987-654-3210', 'MD578750', '2019-08-19');alter
+
+-- 28
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Lily', 'Quinn', 'Anesthesiologist', 'l.quinn@gmail.com', '210-987-6543', 'MD186522', '2018-02-15');
+
+-- 29
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Samuel', 'Wells', 'Radiologist', 's.wells@gmail.com', '678-901-2345', 'MD126532', '2017-01-23');
+
+-- 30
+INSERT INTO MedicalStaff (FirstName, LastName, Specialization, Email, PhoneNumber, LicenseNumber, HireDate)
+VALUES ('Zoey', 'Dixon', 'Urologist', 'z.dixon@gmail.com', '890-123-4567', 'MD176665', '2017-11-28');
